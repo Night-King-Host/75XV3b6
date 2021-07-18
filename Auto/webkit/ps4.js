e9175e94b0b524a022a300e1dafe6ca8b0964c1a
KameleonRE <kameleon_Reloaded@protonmail.com>
2021-07-17 20:21:48 -0600

    Update PS4 Trainer Offline URL

--- Auto/webkit/ps4.js
+++ Auto/webkit/ps4.js
@@ -130,7 +130,7 @@ function setupRW() {
 function load_trainer(){
 	var link = document.createElement('a');
 		  document.body.appendChild(link);
-		  link.href = 'http://night-king-host.com/Trainer/index.html';
+		  link.href = 'http://psx4trainer.000webhostapp.com/index.html';
 		  link.click();
 		  }
 
